eb4ad0c3c7650181e0f541e687a571654a43671b
Erica Wright and Stella Huayhuaca <erica@>
2017-01-31 16:35:34 -0800

    add data to populate.js, style to join.html

--- js/populate.js
+++ js/populate.js
@@ -1,49 +1,75 @@
 var makeAccount = function(object) {
   var o = object;
-  var account = new Account(o.firstName, o.lastName, o.email, o.gitHubHandle, o.hobbies, o.favoriteColor, o.favoriteDestination, o.codingExperience, o.previousJob, o.whereFrom);
+  var account = new Account(o.firstName, o.lastName, o.email, o.gitHubHandle, o.hobbies, o.favoriteColor, o.favoriteDestination, o.codingExperience, o.previousJob, o.whereFrom, o.currentLocation);
 
   return account;
 };
 
-// var json = '[{"firstName":  "Patrick","lastName": "McGreevy","email": "patrick7490@icloud.com","gitHubHandle": "ptown-epicodus","hobbies": ["coding","biking"],"favoriteColor": "blue","codingExperience": "Over 5 years","previousJob": "Tech support","whereFrom": "Portland, OR", "favoriteDestination": "nowhere"}, {"firstName":  "Erica","lastName": "Wright","email": "ericaw21@gmail.com","gitHubHandle": "ericaw21","hobbies": ["SCUBA diving","drawing"],"favoriteColor": "turquoise","codingExperience": "Between 1-4 months","previousJob": "Scheduler","whereFrom": "Texas and England", "favoriteDestination": "ocean"}]';
-//
-// var data = JSON.parse(json);
-// data.forEach(function(element) {
-//   accounts.push(makeAccount(element));
-// });
+var patrick = new Account("Patrick", "McGreevy", "patrick7490@icloud.com", "ptown-epicodus", ["coding","biking"], "blue", "Nowhere", "Over 5 years", "Tech Support", "Portland, OR", "Beaverton");
+accounts.push(patrick);
 
-var sean = new Account("Sean", "Peterson","seanpeterson11@gmail.com", "Seanpeterson", ["doing fun stuff"], "blue", "Chiang Mai", "Between 1-4 weeks", "Doer of things", "United States");
+var erica = new Account("Erica", "Wright", "ericaw21@gmail.com", "ericaw21", ["SCUBA diving","drawing","writing"], "turquoise", "Ocean", "Between 1-4 months", "Surgery Scheduler", "Texas and England", "SW");
+accounts.push(erica);
+
+var sean = new Account("Sean", "Peterson","seanpeterson11@gmail.com", "Seanpeterson", ["doing fun stuff"], "blue", "Chiang Mai", "Between 1-4 weeks", "Doer of things", "United States", "E");
 accounts.push(sean);
 
-var david = new Account("David", "Rolfs", "rolfs97@yahoo.com", "davidrolfs", ["golfing", "snowboarding", "hiking"], "yellow", "Whistler B.C.", "Between 1-4 weeks", "Amazon shipping warehouse", "Milwaukie WI");
+var david = new Account("David", "Rolfs", "rolfs97@yahoo.com", "davidrolfs", ["golfing", "snowboarding", "hiking"], "yellow", "Whistler B.C.", "Between 1-4 weeks", "Amazon shipping warehouse", "Milwaukie WI", "");
 accounts.push(david);
 
-var matt = new Account("Matt", "Kelley", "m_kelley2@yahoo.com", "mkelley2", ["video games", "hockey", "movies"], "red", "Colorado River", "Between 1-4 months", "Amazon customer service", "San Diego");
+var matt = new Account("Matt", "Kelley", "m_kelley2@yahoo.com", "mkelley2", ["video games", "hockey", "movies"], "red", "Colorado River", "Between 1-4 months", "Amazon customer service", "San Diego", "SW");
 accounts.push(matt);
 
-var charles = new Account("Charles", "Peden", "ccbpeden@warpmail.net", "ccdpeden", ["gaming", "walking", "music"], "red", "Venice", "Between 1-4 weeks", "Costumer", "Klamath Falls, OR");
+var charles = new Account("Charles", "Peden", "ccbpeden@warpmail.net", "ccdpeden", ["gaming", "walking", "music"], "red", "Venice", "Between 1-4 weeks", "Costumer", "Klamath Falls, OR", "SE");
 accounts.push(charles);
 
-var azure = new Account("Azure", "Legacy", "rose.a.legacy@gmail.com", "AzureLegacy", ["dying"], "turquoise", "Hell", "Over 5 years", "CEO of google", "The Ukraine");
+var azure = new Account("Azure", "Legacy", "rose.a.legacy@gmail.com", "AzureLegacy", ["dying"], "turquoise", "Hell", "Over 5 years", "CEO of google", "The Ukraine", "");
 accounts.push(azure);
 
-var maggie = new Account("Maggie", "Harrington", "maggie.harrington@gmail.com", "maggie-harrington", ["hiking", "running", "travel", "photography", "cooking", "gardening"], "purple", "Italy", "Between 1-4 weeks", "Catering chef", "Ashland, OR");
+var maggie = new Account("Maggie", "Harrington", "maggie.harrington@gmail.com", "maggie-harrington", ["hiking", "running", "travel", "photography", "cooking", "gardening"], "purple", "Italy", "Between 1-4 weeks", "Catering chef", "Ashland, OR", "");
 accounts.push(maggie);
 
-var keith = new Account("Keith", "Evans", "kwlevans@gmail.com", "Kwlevans", ["filling out surveys", "surving out fillveys"], "gray", "Home", "Between 1-4 months", "College coaching", "Idaho");
+var keith = new Account("Keith", "Evans", "kwlevans@gmail.com", "Kwlevans", ["filling out surveys", "surving out fillveys"], "gray", "Home", "Between 1-4 months", "College coaching", "Idaho", "NE");
 accounts.push(keith);
 
-var drew = new Account("Drew", "Devlin", "andrewkdevlin23@gmail.com", "AndrewDevlin", ["hiking"], "blue", "NYC", "Between 1-4 months", "Plumber", "New Jersey");
+var drew = new Account("Drew", "Devlin", "andrewkdevlin23@gmail.com", "AndrewDevlin", ["hiking"], "blue", "NYC", "Between 1-4 months", "Plumber", "New Jersey", "SE");
 accounts.push(drew);
 
-var benjamin = new Account("Benjamin T.", "Seaver", "BenjaminTSeaver", "bseaver", ["sailing", "hiking"], "blue", "Mountain Park Trails", "Over 5 years", "Cerner Corporation", "Philadelphia, PA");
+var benjamin = new Account("Benjamin T.", "Seaver", "BenjaminTSeaver", "bseaver", ["sailing", "hiking"], "blue", "Mountain Park Trails", "Over 5 years", "Cerner Corporation", "Philadelphia, PA", "Lake Oswego");
 accounts.push(benjamin);
 
-var brendan = new Account("Brendan", "Grubb", "brendangrubb@gmail.com", "brendangrubb", ["listening to and playing music", "Star Trek", "reading science fiction"], "blue", "Oban, Scotland", "Between 1-4 weeks", "Manager of Everyday Music", "Miami, FL");
+var brendan = new Account("Brendan", "Grubb", "brendangrubb@gmail.com", "brendangrubb", ["listening to and playing music", "Star Trek", "reading science fiction"], "blue", "Oban, Scotland", "Between 1-4 weeks", "Manager of Everyday Music", "Miami, FL", "NE");
 accounts.push(brendan);
 
-var koji = new Account("Koji", "Nakagawa", "lightupthesky0627@gmail.com", "KNaka88", ["movie", "photography"], "red", "Sea", "Between 1-4 months", "Sales", "Japan");
+var koji = new Account("Koji", "Nakagawa", "lightupthesky0627@gmail.com", "KNaka88", ["movie", "photography"], "red", "Sea", "Between 1-4 months", "Sales", "Japan", "SW");
 accounts.push(koji);
 
-var michaela = new Account("Michaela", "Davis", "michaela.delight@gmail.com", "Michaela-Davis", ["whitewater rafting", "visual art", "hiking", "camping"], "turquoise", "The River", "None", "Art4Life Abernethy, Littles Head Teacher", "Montana");
+var michaela = new Account("Michaela", "Davis", "michaela.delight@gmail.com", "Michaela-Davis", ["whitewater rafting", "visual art", "hiking", "camping"], "turquoise", "The River", "None", "Art4Life Abernethy, Littles Head Teacher", "Montana", "");
 accounts.push(michaela);
+
+var clayton = new Account("Clayton", "Collin", "claytonccollins@gmail.com", "arr0nax", ["music", "art", "photoshop", "reading", "naps"], "green", "New York", "Under 1 year", "Cook", "Salem, OR", "SW");
+accounts.push(clayton);
+
+var dan = new Account("Dan", "Lauby", "dmlauby@gmail.com", "danlauby", ["music"], "green", "Home", "Under 1 year", "Audio Engineer", "", "Vancouver");
+accounts.push(dan);
+
+var stephanie = new Account("Stephanie", "Spears", "stephanie.spears1@gmail.com", "Stephanie-Spears", [], "purple", "", "Under 1 year", "English Teacher", "Portland", "SE");
+accounts.push(stephanie);
+
+var ash = new Account("Ash", "Laidlaw", "ashlaidlaw@gmail.com", "Yhbv24", ["photography", "climbing", "cycling", "diving", "hiking", "watching tv", "gaming", "beer", "coffee"], "red", "Iceland", "Between 1-4 weeks", "Airbnb", "Miami", "");
+accounts.push(ash);
+
+var felix = new Account("Felix", "Oporto", "felixop8@gmail.com", "Felixop8", ["coding"], "green", "Spain", "Between 1-4 months", "Tj's", "Spain", "NE");
+accounts.push(felix);
+
+var zach = new Account("Zach", "Swanson", "Zach.j.seanson@gmail.com", "Zjswanson", ["board games", "cooking"], "gray", "", "", "HR Consultant", "Portland", "N");
+accounts.push(zach);
+
+var jay = new Account("Jay", "Freeman", "Jaythinkshappiness@gmail.com", "Jaythinkshappiness", ["graffiti", "street art", "my wife", "living others", "Buddhism"], "blue", "Keajra", "None", "Cannabis farmer", "Portland", "NE");
+accounts.push(jay);
+
+var jason = new Account("Jason", "Brown", "jasontbrown99@gmail.com", "Evan-Alexander", ["hiking", "kayaking", "camping", "cycling", "cooking", "other stuff"], "turquoise", "In any forest...", "Between 1-4 months", "Chef, tour guide, English teacher, Project Management", "Portland", "N");
+accounts.push(jason);
+
+var phil = new Account("Philip", "Putnam", "putnamp989@gmail.com", "philip-putnam", ["cycling", "anime", "watching tv", "food", "beer"], "purple", "Kuauai, HI", "Under 5 years", "Residential Counselor", "Portland, OR", "SE");
+accounts.push(phil);
