ddc6041c75365b8a6f86efc3a5cd8ed705d29db6
Erica Wright and Philip Putnam <erica@>
2017-02-02 14:47:30 -0800

    add skeleton game functionality

--- js/game-scripts.js
+++ js/game-scripts.js
@@ -2,6 +2,8 @@
 function Game(numberOfAccounts) {
   this.studentCards = [];
   this.numberOfAccounts = numberOfAccounts;
+  this.turn = 1;
+  this.flip = 0;
 }
 
 Game.prototype.clearCards = function() {
@@ -9,6 +11,26 @@ Game.prototype.clearCards = function() {
 }
 Game.prototype.addCard = function(accountItems) {
   this.studentCards.push(accountItems);
+}
+Game.prototype.calcFlip = function() {
+  if (this.flip === 0) {
+    this.flip = 1;
+  } else {
+    this.flip++;
+  }
+  return this.flip;
+}
+
+function comparison(flipClass1, flipClass2) {
+  if (flipClass1 !== flipClass2) {
+    this.turn = 1;
+    this.flip = 0;
+    return false;
+  } else if (flipClass1 === flipClass2) {
+    this.turn++;
+    this.flip = 0;
+    return true;
+  }
 }
 
 function random(max) {
@@ -49,8 +71,10 @@ $(document).ready(function() {
 
   $("#new-game").submit(function(event) {
     event.preventDefault();
+    $("div#game-board").empty();
     var userInput = $(".student-number").val();
     var game = new Game(userInput);
+    var comparFlip1 = "", comparFlip2 = "";
     game.clearCards();
 
     for (var index = 1; index <= userInput; index ++) {
@@ -77,8 +101,20 @@ $(document).ready(function() {
 
 
       $("div#game-board div").last().click(function() {
-        $(this).children("img, p").show();
+        $(this).children("img, p").toggle();
+        if (!comparFlip1) {
+          comparFlip1 = $(this).last().attr("class");
+        } else if (!comparFlip2) {
+          comparFlip2 = $(this).last().attr("class");
+        }
+        game.calcFlip(comparFlip1, comparFlip2);
+
+        if (comparison(comparFlip1, comparFlip2)) {
+          comparFlip1 = "";
+          comparFlip2 = "";
+        }
       });
+
 
     }); // end of forEach function
 
