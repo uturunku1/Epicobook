fb7afc972dbe678acab12aee8f67a960c60a94ee
Erica Wright and Philip Putnam <philip@>
2017-02-01 16:43:49 -0800

    add array for student name and pictures to randomly generate

--- js/game-scripts.js
+++ js/game-scripts.js
@@ -1,3 +1,12 @@
+//Back-end
+function random(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+
+
+//Front-end
 var selectStudents = function() {
   var accountTotal = accounts.length;
   for (var index = 2; index <= accountTotal; index ++) {
@@ -5,19 +14,22 @@ var selectStudents = function() {
   }
 };
 
-
-
-
 $(document).ready(function() {
   selectStudents();
+  var studentCards = [];
 
   $("#new-game").submit(function(event) {
     event.preventDefault();
+
+    studentCards = [];
     var userInput = $(".student-number").val();
     for (var index = 1; index <= userInput; index ++) {
-      $("#game-board").append("<div>Hello</div>");
+      var tempRandomNumber = random(accounts.length);
+      studentCards.push(accounts[tempRandomNumber].pictureURL);
+      studentCards.push(accounts[tempRandomNumber].firstName);
+      alert(studentCards);
+      // $("#game-board").append("<div>Hello</div>");
     }
-
 
 
   });
